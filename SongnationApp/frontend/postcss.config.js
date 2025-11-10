8dd054d94bf7a69703095113a8a60f76608105c0
cauanmguerreiro <cauanguerreiro@hotmail.com>
2025-11-10 19:41:52 -0300

    Merge pull request #11 from Cauanmguerreiro/Estrutura-Inicial-de-Vue,js-e-Quasar adição pasta backend

--- SongnationApp/frontend/postcss.config.js
+++
@@ -1,5 +0,0 @@
-// PostCSS configuration removed.
-// This project was converted from a Quasar app into a minimal static homepage.
-// PostCSS is no longer required here. If you need PostCSS later, recreate this file.
-
-export default {}
